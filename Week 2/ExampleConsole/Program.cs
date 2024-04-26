179c1feede92ab7e3949a776bcd4335a3fd0db42
snc5003 <snc5003@gmail.com>
2024-04-26 16:58:23 -0400

    modifying constructor stuff

--- Week 2/ExampleConsole/Program.cs
+++ Week 2/ExampleConsole/Program.cs
@@ -8,13 +8,13 @@ public class thinga_ma_bobs
     //We are creating a class and here we are defining the fields in that class
     string AttributeColor = "You can a set the class default color";
     
-   //Constructor 1:  Implied, Hidden No-Args or No Arguments Constructor
-   //Automatically generated blank constructor that's hidden
-   //Gets created for every class you make without you having to type it or even see it
-   //But you CAN type it if you want to
-   //public thinga_ma_bobs(){}
-   //If you create another constructor for this class, you will override/eliminate this
-   //hidden, implicit constructor altogether.
+    //Constructor 1:  Implied, Hidden No-Args or No Arguments Constructor
+    //Automatically generated blank constructor that's hidden
+    //Gets created for every class you make without you having to type it or even see it
+    //But you CAN type it if you want to
+    //public thinga_ma_bobs(){}
+    //If you create another constructor for this class, you will eliminate this
+    //hidden, implicit constructor altogether.
   
     //Constructor 2:
     //Here we are going to set the rules for how to create an object of type thinga_ma_bobs
@@ -32,7 +32,9 @@ public class thinga_ma_bobs
     //without requiring that they provide a string for UserInputColor, we can explicitly
     //write out the blank constructor for this clsas.
     public thinga_ma_bobs()
-    {}
+    {
+
+    }
 
     //This is an example of method overloading.  Method overloading is when you
     //create a method that has a set of parameters (or none at all) and then you creae
@@ -47,13 +49,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        //Use Constructor 3
-        thinga_ma_bobs mynewthinga_ma_bob = new thinga_ma_bobs(); //thingsa_ma_bobs() is calling the thinga_ma_bobs constructor
-        //Console.WriteLine(mynewthinga_ma_bob);
 
         //Use Constructor 2 (you have to include a color here)
         thinga_ma_bobs Pinkthinga_ma_bob = new thinga_ma_bobs("pink");
-        Console.WriteLine(Pinkthinga_ma_bob);
+
+        //Use Constructor 3
+        thinga_ma_bobs mynewthinga_ma_bob = new thinga_ma_bobs(); //thingsa_ma_bobs() is calling the thinga_ma_bobs constructor
 
 
 
