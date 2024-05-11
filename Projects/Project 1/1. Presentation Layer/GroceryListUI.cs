74dd62fd3dd1bf9f5a8e6c0947511cf4fb3cc8bf
snc5003 <snc5003@gmail.com>
2024-05-11 14:44:41 -0400

    formatting changes

--- Projects/Project 1/1. Presentation Layer/GroceryListUI.cs
+++ Projects/Project 1/1. Presentation Layer/GroceryListUI.cs
@@ -10,12 +10,22 @@ public static Guid DisplayGroceryList (Guid userId, MealPlans userMealPlan)
     {
     
         Console.WriteLine("Here's your grocery list!");
+
+        Console.WriteLine("Item Name\t\t\tQuantity");
         List<GroceryItems> compiledGroceryList = GroceryListLogic.GetGroceryList(userMealPlan);
         for (int i = 0; i < compiledGroceryList.Count(); i++)
         {
-            Console.WriteLine($"{compiledGroceryList[i].itemName}\t\t\t {compiledGroceryList[i].quantity} {compiledGroceryList[i].unitOfMeasure}");
+            if(compiledGroceryList[i].itemName.Length>=16)
+            {Console.WriteLine($"{compiledGroceryList[i].itemName}\t\t{compiledGroceryList[i].quantity} {compiledGroceryList[i].unitOfMeasure}");}
+            else if (compiledGroceryList[i].itemName.Length>=8)
+            {Console.WriteLine($"{compiledGroceryList[i].itemName}\t\t\t{compiledGroceryList[i].quantity} {compiledGroceryList[i].unitOfMeasure}");}
+
+            else
+            {Console.WriteLine($"{compiledGroceryList[i].itemName}\t\t\t\t{compiledGroceryList[i].quantity} {compiledGroceryList[i].unitOfMeasure}");}
 
         }
+
+
         return userId;
     }
 }
